1a47fb1380455cdeab73b9b735ecccd8bfa336a4
Kalicz Péter <kaliczp@gmail.com>
2025-03-27 13:27:01 +0100

    Ismétlés matrixig

--- Bevezet.R
+++ Bevezet.R
@@ -1,1 +1,12 @@
 ### Ismétlés
+a <- 1:50
+str(a)
+b <- "Kalicz"
+str(b) # Objektum struktúrája
+length(a)
+c <- diag(10)
+c[2,2] # Indexelés
+?matrix # Kézikönyv!
+d <- matrix(data = 1:4, nrow = 2)
+d # Fent létrehozott mátrix kiíratása
+    
