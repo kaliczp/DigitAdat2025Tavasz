6cb5332732285a53badd6f7bb8c905b1828539df
Kalicz Péter <kaliczp@gmail.com>
2025-03-27 14:07:11 +0100

    Logika vektor

--- Bevezet.R
+++ Bevezet.R
@@ -18,3 +18,16 @@ c("A","G") # Karakter vektor létrehozása
 e <- data.frame(V1 = c(1,3), V2 = c(2,4)) # data.frame létrehozása
 data.frame(V1 = c(1,3), V2 = c(2,4), Bet = c("A","G")) # újabb oszlop
 cbind(e, Bet = c("A","G")) # Ugyan az, mint az előző más függvénnyel
+rbind(e, Bet = c("A","G")) # Sor hozzáfűzése. Figyelem, így minden karakter lesz! Az oszlopok, azonos típusúak legyenek!
+
+cbind(e, "A") # Automatikus kiegészítés, az egész oszlop A betű
+cbind(e, TRUE) # Automatikus kiegészítés, az egész oszlop TRUE
+
+## Vektorizált műveletek
+e + 2 # Mátrix összes eleméhez hozzáad kettőt
+a + 2 # Vektorokra is megy
+
+## Logikai kifejezések ez is vektorizált
+a > 10 # Objektumra is működnek
+e$V2 > 2 # Második változóm kiértékelése
+
