bda8fca56c624a9ded55cd1cba20187cd05aa153
Kalicz Péter <kaliczp@gmail.com>
2025-04-24 15:15:13 +0200

    Javított beolvasás és sum

--- Mihaly.R
+++ Mihaly.R
@@ -1,3 +1,10 @@
 ## install.packages("readxl")
 library(readxl)
-Osszesito <- as.data.frame(read_excel("KosztaM_osszesito_tablat_20221102_javitott (1).xlsx"))
+## Beolvasás adattáblába (data.frame)
+Osszesito <- as.data.frame(read_excel("KosztaM_osszesito_tablat_20221102_javitott (1).xlsx"),
+                           n_max = 25)
+## Javított beolvasás
+Osszesito <- as.data.frame(read_excel("KosztaM_osszesito_tablat_20221102_javitott (1).xlsx",
+                                      range = "A1:IX25"))
+## A teljes vizsgált terület
+sum(Osszesito$"Poligon területe (m2)")
