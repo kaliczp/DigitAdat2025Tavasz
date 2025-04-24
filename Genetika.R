8ee6fd942434b57a232d7e9f7d5e68ab33a6d8c0
Kalicz Péter <kaliczp@gmail.com>
2025-04-24 16:00:50 +0200

    Install with dependencies comments

--- Genetika.R
+++ Genetika.R
@@ -1,5 +1,8 @@
 ## adegenet csomag
 ## install.packages("adegenet")
+## install.packages("adegenet", dep=TRUE) # a függőségekkel is.
 library(adegenet)
 adegenetWeb() # web page opening
 ?adegenet
+
+### A tutorial alapján haladunk
