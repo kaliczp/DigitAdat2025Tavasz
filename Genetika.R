13cc8ec96c661bb049ada73e615a4e3e9cd7ccb7
Kalicz Péter <kaliczp@gmail.com>
2025-04-24 16:57:52 +0200

    Importálható data.frame előállítása

--- Genetika.R
+++ Genetika.R
@@ -14,3 +14,16 @@ nancycats@tab
 
 ## Import próba
 PET <- read.csv2("INPUT20250310taxgeo.csv", skip = 2)
+## Két oszlop összeforrasztása lókuszonként
+loci <- data.frame(apply(PET[,3:4], 1, paste, collapse = "/"))
+for(lokusz in 3:22){
+    lok.akt <- 2*lokusz
+    akt <- data.frame(apply(PET[,(lok.akt-1):lok.akt], 1, paste, collapse = "/"))
+    loci <- cbind(loci,akt)
+}
+
+## Lokusz nevek
+PET.names <- as.character(read.csv2("INPUT20250310taxgeo.csv", skip = 2, head = FALSE, nrows = 1))
+colnames(loci) <- c(PET.names[!PET.names == "NA"])
+## Egyedek elnevezése
+row.names(loci) <- PET[,ncol(PET)]
