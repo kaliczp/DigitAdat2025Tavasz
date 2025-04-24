7f8c71f4e06463bb015a575fd0bb66aa6b36096a
Kalicz Péter <kaliczp@gmail.com>
2025-04-24 15:26:28 +0200

    Indexelés és magyarázat

--- Mihaly.R
+++ Mihaly.R
@@ -6,5 +6,12 @@ Osszesito <- as.data.frame(read_excel("KosztaM_osszesito_tablat_20221102_javitot
 ## Javított beolvasás
 Osszesito <- as.data.frame(read_excel("KosztaM_osszesito_tablat_20221102_javitott (1).xlsx",
                                       range = "A1:IX25"))
-## A teljes vizsgált terület
-sum(Osszesito$"Poligon területe (m2)")
+## Megvan az objektum?
+ls()
+## A teljes vizsgált terület kétféle indexszeléssel
+sum(Osszesito$"Poligon területe (m2)") # Dollár után oszlopnév
+sum(Osszesito[,"Poligon területe (m2)"]) # Indexként használt oszlopnév
+sum(Osszesito[,3]) # A harmadik oszlop
+## A területek öszegzése
+quantile(Osszesito[,"Poligon területe (m2)"])
+summary(Osszesito[,"Poligon területe (m2)"])
