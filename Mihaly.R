1d99e9276aa4e9ad330c60d142653b26046475c6
Kalicz Péter <kaliczp@gmail.com>
2025-04-24 15:00:59 +0200

    Mentés data.frameként

--- Mihaly.R
+++ Mihaly.R
@@ -1,3 +1,3 @@
 ## install.packages("readxl")
 library(readxl)
-read_excel("KosztaM_osszesito_tablat_20221102_javitott (1).xlsx")
+Osszesito <- as.data.frame(read_excel("KosztaM_osszesito_tablat_20221102_javitott (1).xlsx"))
